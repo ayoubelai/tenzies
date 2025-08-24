d037eceac17fac1f97fd948470d39e41a0f66aae
Ayoub-Ell <aelai054@uottawa.ca>
2025-08-24 00:05:47 -0400

    Make dice render from mapped dice arr

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,25 @@
+import React from "react"
 import Die from "./Die"
 
 export default function App() {
+  const [dice, setDice] = React.useState(generateAllNewDice())
 
   function generateAllNewDice() {
     const dice = []
-    for (let i = 0; i <= 10; i++){
-      dice.push(Math.floor(Math.random() * 6)+1)
+    for (let i = 0; i < 10; i++){
+      dice.push(Math.floor(Math.random() * 6) + 1)
     }
     return dice
   }
 
+  const diceItems = dice.map(die => (
+    <Die key={die} value={die}/>
+  ))
+
   return (
     <main>
       <div className="diceContainer">
-        <Die value="1"/>
-        <Die value="6"/>
-        <Die value="4"/>
-        <Die value="5"/>
-        <Die value="2"/>
-        <Die value="3"/>
-        <Die value="5"/>
-        <Die value="6"/>
-        <Die value="2"/>
-        <Die value="1"/>
+        {diceItems}
       </div>
     </main>
   )
